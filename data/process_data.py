ef76a91a720756722501e650671626ec553a9176
EmrahDemirezen <47589958+EmrahDemirezen@users.noreply.github.com>
2022-07-09 13:42:27 +0300

    Update process_data.py

--- data/process_data.py
+++ data/process_data.py
@@ -26,6 +26,8 @@ def clean_data(df):
         categories[column] = categories[column].str.split("-",expand = False).str[1]
     # convert column from string to numeric
         categories[column] = categories[column].astype('int')
+    # convert to 1 all values highr than 1  
+        categories[column] = categories[column].apply(lambda x: 1 if x < 1)
     # drop the original categories column from `df`
     df=df.drop(["categories"], axis=1)
     # concatenate the original dataframe with the new `categories` dataframe
